391065922e463014191ffd085d3eee286cf17abf
Yoon-Suji <70956926+Yoon-Suji@users.noreply.github.com>
2022-03-25 00:48:42 +0900

    Refactor: Fix ModelRequestDto

--- src/main/java/com/fedi/web/dto/ModelRequestDto.java
+++ src/main/java/com/fedi/web/dto/ModelRequestDto.java
@@ -6,11 +6,13 @@ import lombok.Builder;
 
 public class ModelRequestDto {
 	private Long imageId;
-	private Double vector;
+//	private Double vector;
+	private String imageUrl;
 	
 	@Builder
 	public ModelRequestDto(Image image) {
 		this.imageId = image.getImageId();
-		this.vector = image.getVector();
+//		this.vector = image.getVector();
+		this.imageUrl = image.getImageUrl();
 	}
 }
