47a8bc2cd7edcc26536838ae079609278c7365dd
Yoon-Suji <70956926+Yoon-Suji@users.noreply.github.com>
2022-03-30 16:33:42 +0900

    Refactor: /results api 연결

--- src/main/java/com/fedi/service/ResultService.java
+++ src/main/java/com/fedi/service/ResultService.java
@@ -45,7 +45,7 @@ public class ResultService {
 		restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
 		
 		JSONParser jsonParser = new JSONParser();
-		String url = "http://0b4f-221-147-94-92.ngrok.io/analysis";
+		String url = "http://localhost/results";
 		
 		HttpHeaders httpHeaders = new HttpHeaders(); //Header 생성
 		httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
