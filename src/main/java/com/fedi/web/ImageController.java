accd2c33f445aeba5e2ff2a101838f9270ad3447
Yoon-Suji <jidl88@ewhain.net>
2022-05-04 22:26:54 +0900

    Fix: import 정리

--- src/main/java/com/fedi/web/ImageController.java
+++ src/main/java/com/fedi/web/ImageController.java
@@ -1,30 +1,18 @@
 package com.fedi.web;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fedi.service.ImageService;
 import com.fedi.web.dto.ImageRequestDto;
-import com.fedi.web.dto.LikeRpaResponseDto;
 import com.fedi.web.dto.VectorResponseDto;
-import com.google.gson.Gson;
 
 import lombok.RequiredArgsConstructor;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
